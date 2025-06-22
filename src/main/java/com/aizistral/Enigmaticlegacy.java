52c4dd7f87ef9b824a45946fe810951a60af1476
unknown <kms1184@rit.edu>
2025-06-22 12:31:45 -0400

    basic asset pulling

--- src/main/java/com/aizistral/Enigmaticlegacy.java
+++ src/main/java/com/aizistral/Enigmaticlegacy.java
@@ -8,9 +8,6 @@ import org.slf4j.LoggerFactory;
 public class Enigmaticlegacy implements ModInitializer {
 	public static final String MOD_ID = "enigmaticlegacy";
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
@@ -19,6 +16,7 @@ public class Enigmaticlegacy implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Loading EnigmaticLegacy Fabric Version");
 	}
+
 }
